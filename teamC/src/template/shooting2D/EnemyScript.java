dd14559bc37ce20ef5cebddd7fb624ba9908e99c
s1771017 <s1771017@d-13102-050.ku-kinds.konan-u.ac.jp>
2018-10-26 15:50:07 +0900

    敵スクリプトを触りました。まだ動くようにはなっていません。

--- teamC/src/template/shooting2D/EnemyScript.java
+++ teamC/src/template/shooting2D/EnemyScript.java
@@ -1,5 +1,6 @@
 package template.shooting2D;
 import framework.game2D.Sprite;
+import framework.game2D.Velocity2D;
 
 
 public class EnemyScript extends Sprite {
@@ -17,13 +18,22 @@ public class EnemyScript extends Sprite {
 	int rangeWidth = TemplateShooting2D.RANGE;
 	int rangeHeight = TemplateShooting2D.RANGE;
 	//敵のインスタンスを生成する際に、ステータスを設定する
-	//敵の画像,敵の体力,
+	//敵の画像,敵の体力,速度,角度
 	public EnemyScript(String imageFile,int _enemyHP,double _speed,double _angle) {
 		super(imageFile);
 		enemyHP=_enemyHP;
 	}
-	public void motion() {
+	public void motion(long interval) {
+		Velocity2D vel=this.getVelocity();
+		vel.set(Math.cos(angle)*enemySpeed,Math.sin(angle)*enemySpeed);
+		setVelocity(vel);
+		super.motion(interval);
 
+	}
+	public void destroy() {
+		if(enemyHP<=0) {
+
+		}
 	}
 
 
